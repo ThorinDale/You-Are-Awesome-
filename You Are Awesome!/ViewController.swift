b03809ab61513f23f8e0f05e18f611dc7473641b
Darren Zheng <zhengdarren9@gmail.com>
2021-02-03 18:28:04 -0500

    add images and UIImage in Storyboard

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -10,17 +10,16 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("👍 viewDidLoad has run!")
-        messageLabel.text = "Fabulous? That's You!"
     }
     
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        print("😎 The message button was pressed!")
         messageLabel.text = "You Are Awesome!"
         messageLabel.textColor = UIColor.blue
+        imageView.image = UIImage(named: "image0")
     }
     
     @IBAction func anotherMessageButtonPressed(_ sender: UIButton) {
